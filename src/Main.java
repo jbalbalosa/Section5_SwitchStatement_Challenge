97a8f58736d0f3888f92a2aa2be724caa19d9999
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-23 14:49:40 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("Section 5: Switch Statements Challenge+");
+
+        char swChar = 'C';
+        switch(swChar) {
+            case 'A':
+                System.out.println("Character is A");
+                break;
+            case 'B':
+                System.out.println("Character is B");
+                break;
+            case 'C':
+                System.out.println("Character is C");
+                break;
+            case 'D':
+                System.out.println("Character is D");
+                break;
+            case 'E':
+                System.out.println("Character is E");
+                break;
+            default:
+                System.out.println("Was Not found!");
+                break;
+        }
+
+        String month="January";
+        switch(month.toLowerCase()){
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Not sure");
+        }
     }
 }
